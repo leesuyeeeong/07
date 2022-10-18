da51f553a5488efd9a47aafcbab42ebe160218f2
leesuyeeeong <1913839@sookmyung.ac.kr>
2022-10-18 16:42:30 +0900

    실습4

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) 
+int counter;
+
+void set_counter()
 {
-	int i = 10;
-	pfintf("함수 호출전 i=%d\n",i);
-	inc(i);
-	pfintf("함수 호출후 i=%d\n",i);
-	return 0; 
+	counter=20;
+}
+int main(void)
+{
+	printf("counter=%d\n",counter);
+	set_counter();
+	printf("counter=%d\n",counter);
+	return 0;
 }
 
-int inc(int counter)
-{
-	counter++;
-	return counter;
-}
