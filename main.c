a5a89c16143795a5954568f0e6ecb8b89001c59f
leesuyeeeong <1913839@sookmyung.ac.kr>
2022-10-18 16:39:23 +0900

    실습3

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	int i;
-	
-	for (i=0;i<5;i++)
-	{
-		int temp = 1;
-		printf("temp = %d\n", temp);
-		temp++;
-	}
-	return 0;
+int main(int argc, char *argv[]) 
+{
+	int i = 10;
+	pfintf("함수 호출전 i=%d\n",i);
+	inc(i);
+	pfintf("함수 호출후 i=%d\n",i);
+	return 0; 
+}
+
+int inc(int counter)
+{
+	counter++;
+	return counter;
 }
